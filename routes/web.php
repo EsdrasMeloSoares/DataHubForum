5bab4cc1c7e9d7d88a847e511f1d5ab645dd293e
EsdrasMeloSoares <esdrasmelo12soares@gmail.com>
2025-10-31 14:06:33 -0300

    Add new routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Livewire\Users\Index as UsersIndex;
 use App\Livewire\Posts\Index as PostsIndex;
+use App\Livewire\Post\Show as PostShow;
 use App\Livewire\User\Profile;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,8 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/users', UsersIndex::class)->name('users.index');
     Route::get('/posts', PostsIndex::class)->name('posts.index');
+
+    Route::get('/post/{post}', PostShow::class)->name('post.show');
 
     Route::get('/user/profile', Profile::class)->name('user.profile');
 });
